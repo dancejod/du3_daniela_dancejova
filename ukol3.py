049fbd256cdca2215ef68663ae30a23f898d6fa4
dancejod <dancejova.daniela@gmail.com>
2021-12-17 01:35:15 +0100

    Niektore riadky kodu skratene. Teraz kod vyhodnocuje aj kontajnery pristupne iba obyvatelom domu

--- ukol3.py
+++ ukol3.py
@@ -15,29 +15,39 @@ with open ("adresy.geojson", encoding="utf-8") as adresy, open("kontejnery.geojs
     
     data_adresy = json.load(adresy)                 ### Nacitanie dat zo suborov
     data_kontajnery = json.load(kontajnery)
+
+    print("Nacitanych {pocet_adries} adresnych bodov.".format(pocet_adries = len(data_adresy["features"])))
+    print("Nacitanych {pocet_kontajnerov} kontajnerov.".format(pocet_kontajnerov = len(data_kontajnery["features"])))
+    print("Program teraz vypocitava vzdialenosti vchodov od kontajnerov . . .")
     
-    for adresa in range(len(data_adresy["features"])):                                  ### Pre kazdu adresu sa na zaciatok nacitaju jej suradnice zo suboru a transformuju sa do S-JTSK
-        krovak_x = data_adresy["features"][adresa]["geometry"]["coordinates"][0]
-        krovak_y = data_adresy["features"][adresa]["geometry"]["coordinates"][1]       
+    for adresa in data_adresy["features"]:                                  ### Pre kazdu adresu sa na zaciatok nacitaju jej suradnice zo suboru a transformuju sa do S-JTSK
+        krovak_x = adresa["geometry"]["coordinates"][0]
+        krovak_y = adresa["geometry"]["coordinates"][1]       
         krovak = wgs2jtsk.transform(krovak_x,krovak_y)
         
-        for kontajner in range(len(data_kontajnery["features"])):                               ### Cyklus pre prechadzanie kazdym kontajnerom
-            if data_kontajnery["features"][kontajner]["properties"]["PRISTUP"] == "volně":
-                dlz_x = data_kontajnery["features"][kontajner]["geometry"]["coordinates"][0]
-                dlz_y = data_kontajnery["features"][kontajner]["geometry"]["coordinates"][1]
+        for kontajner in data_kontajnery["features"]:                               ### Cyklus pre prechadzanie kazdym kontajnerom
+            if kontajner["properties"]["PRISTUP"] == "volně":
+                dlz_x = kontajner["geometry"]["coordinates"][0]
+                dlz_y = kontajner["geometry"]["coordinates"][1]
                 vzdialenost = int(sqrt((krovak[0]-dlz_x)**2+(krovak[1]-dlz_y)**2))              ### Vypocet vzdialenosti cez Pytagorovu vetu
 
                 if pomocna_vzdialenost == None or pomocna_vzdialenost > vzdialenost:
                     pomocna_vzdialenost = vzdialenost                                           ### Zachovanie najmensej vzdialenosti
 
-        
-        data_adresy["features"][adresa]["k_najblizsiemu_kontajneru"] = pomocna_vzdialenost      ### Do slovnika sa k danej adrese pripise novy kluc s najmensou vzdialenostou
+            elif kontajner["properties"]["PRISTUP"] == "obyvatelům domu":
+                aktualna_adresa = "{ulica} {cislo}".format(ulica = adresa["properties"]["addr:street"], cislo = adresa["properties"]["addr:housenumber"])
+                if aktualna_adresa == kontajner['properties']['STATIONNAME']:
+                    pomocna_vzdialenost = 0
+                else:
+                    pass
+
+        adresa["k_najblizsiemu_kontajneru"] = pomocna_vzdialenost      ### Do slovnika sa k danej adrese pripise novy kluc s najmensou vzdialenostou
         pomocna_vzdialenost = None                                                              ### Pomocna vzdialenost sa vynuluje pre pracu s dalsou adresou
     
     vzdialenosti = [adresa["k_najblizsiemu_kontajneru"] for adresa in data_adresy["features"]]  ### Premenna, do ktorej sa nacita zoznam vypocitanych najmensich vzdialenosti
-    
-    najdi_index = (vzdialenosti.index(max(vzdialenosti)))
+    print(vzdialenosti)
+    najdi_index = (vzdialenosti.index(max(vzdialenosti)))                                       ### Najde sa index hodnoty s najvacsou vzdialenostou od kontajneru, aby sa tak nasla aj adresa miesta
 
-    print(f"Priemerna vzdialenost ku kontajnerom je: {round(mean(vzdialenosti),2)}.")            
-    print(f"Median vzdialenosti ku kontajnerom je: {int(median(vzdialenosti))}.")                ### Pouzitie kniznice statistics pre jednoduchy priemer a median
+    print(f"Priemerna vzdialenost ku kontajnerom je: {round(mean(vzdialenosti),1)}")            
+    print(f"Median vzdialenosti ku kontajnerom je: {int(median(vzdialenosti))}")                ### Pouzitie kniznice statistics pre jednoduchy priemer a median
     print("Najdalej od kontajneru vo vzdialenosti {max_vzdialenost} je vchod na adrese {adresa} {cd}".format(max_vzdialenost = max(vzdialenosti),adresa = data_adresy["features"][najdi_index]["properties"]["addr:street"],cd = data_adresy["features"][najdi_index]["properties"]["addr:housenumber"]))
